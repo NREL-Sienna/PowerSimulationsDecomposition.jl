f68f005f0082045c5a6cce8bf36378ccae53078e
m-bossart <bossart.matt@gmail.com>
2025-12-09 17:32:52 -0800

    improved printing for multiregion problem

--- src/print.jl
+++ src/print.jl
@@ -1,7 +1,15 @@
-function Base.show(io::IO, ::MIME"text/plain", input::MultiProblemTemplate)
-    println(io, "Print somenthing clever here. Template")
+function Base.show(io::IO, x::MIME"text/plain", input::MultiProblemTemplate)
+    for (k, template) in input.sub_templates
+        println(io, "SUBTEMPLATE $k:")
+        Base.show(io::IO, x, template)
+        println(io, "\n")
+    end
 end
 
-function Base.show(io::IO, ::MIME"text/plain", input::PSI.DecisionModel{MultiRegionProblem})
-    println(io, "Print somenthing clever here. Problem")
+function Base.show(
+    io::IO,
+    x::MIME"text/plain",
+    input::PSI.DecisionModel{MultiRegionProblem},
+)
+    Base.show(io::IO, x, input.template)
 end
